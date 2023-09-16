663d73a52cbb752dd10a12171e6ca397f2565fd9
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-09-16 17:14:49 -0300

    called pokemon api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,39 @@
-export default function Home() {
-    return <h1>Pokedex</h1>
+interface Pokemon {
+    name: "string",
+    url: "string",
+    id: number
+  
+  }
+  
+  export async function getStaticProps() {
+  
+    const maxNumberOfPokemons = 500;
+    const api = 'https://pokeapi.co/api/v2/pokemon';
+  
+    const res = await fetch(`${api}/?limit=${maxNumberOfPokemons}`)
+    const data = await res.json();
+
+    data.results.forEach((item: Pokemon, index: number) => {
+      item.id = index + 1;
+    })
+  
+    return {
+      props: {
+        pokemons: data.results,
+      }
+    }
+  
+  }
+
+export default function Home( {pokemons} ) {
+    return (
+        <>
+            <h1>Pokedex</h1>
+            <ul>
+                {pokemons.map(pokemon => (
+                    <li key={pokemon.id}>{pokemon.name}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
